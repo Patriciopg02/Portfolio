f11126fdd11157f48cc1ae51119a0ee93170f8c0
Patriciopg02 <patriciopg02_@hotmail.com>
2022-11-10 00:32:24 -0300

    landing-finished

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,13 +2,16 @@ import './Landing.css';
 
 export default function Landing() {
     return (
-        <div className='landing'>
-        <div className='text'>
-          <h3>Welcome 👋</h3>
-          <h2>I'm Patricio Pereyra Gargiulo</h2>
-          <h3 id='orange'>Fullstack Web Developer</h3>
-        </div>
-        <a href='#'>Contact me</a>
+        <div className='landing' align='center'>
+          {/* <div id='animation1'> </div> */}
+          <div className='textLanding'>
+            <h1 id='welcome'>¡Welcome!</h1>
+            <h1 id='name'>Patricio Pereyra Gargiulo</h1>
+            <div id='developerDIV'>
+              <span id='developer'>Full Stack Developer</span>
+            </div>
+          </div>
+            <div id='flecha'> </div>
         </div>
     )
 }
