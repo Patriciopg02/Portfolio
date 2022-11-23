3f7bf9074588d74a4f0a3b0426a3b7539b684dcd
Patriciopg02 <patriciopg02_@hotmail.com>
2022-11-23 14:27:17 -0300

    added-switchtheme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import {GeneralContainer, Landing, Themes} from './components';
 import {ThemeProvider} from 'styled-components'
+import { useState } from 'react';
+import Switch from './components/Switch/Switch';
 
 
 function App() {
+  const [theme, setTheme] = useState('Light');
+
   return (
     <div className="App">
-      <ThemeProvider theme={Themes['Light']}>
-        <Landing theme={Themes['Light']}/>
+      <ThemeProvider theme={Themes[theme]}>
+        <Switch theme={theme} setTheme={setTheme}/>
+        <Landing theme={Themes[theme]} setTheme={setTheme}/>
         <GeneralContainer/>
       </ThemeProvider>
     </div>
